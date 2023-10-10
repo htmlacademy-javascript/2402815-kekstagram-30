aba71884af11a8dd1ddc4292f3bf3e255dcb7ae2
Keks <keks@htmlacademy.ru>
2023-10-10 23:47:20 +0300

    :heavy_check_mark: Сборка #2

---
+++ 2/js/functions.js
@@ -0,0 +1,17 @@
+const isShortString = (string, countOfSymbols) => {
+  return countOfSymbols <= string.length;
+  };
+
+isShortString('проверяемая строка', 20);
+isShortString('проверяемая строка', 18);
+isShortString('проверяемая строка', 10);
+
+const isPalindrom = (string) => {
+  let phrase = string.replaceAll(' ', '').toLowerCase();
+  let reversePhrase = "";
+  for (let i = phrase.length-1; i >= 0; i--){
+    reversePhrase += phrase[i];
+  }
+return reversePhrase === phrase ? "Палиндром" : "Не палиндром"
+}
+isPalindrom('Лёша на полке клопа нашёл ');
