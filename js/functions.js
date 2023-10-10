14ca32951dfbbd68f364a54858ea15102ed7e137
hect11c <svetlana1korolkova@gmail.com>
2023-10-10 23:53:29 +0300

    Нужно больше функций

--- js/functions.js
+++ js/functions.js
@@ -1,17 +1,15 @@
-const isShortString = (string, countOfSymbols) => {
-  return countOfSymbols <= string.length;
-  };
+const isShortString = (string, countOfSymbols) => countOfSymbols <= string.length;
 
 isShortString('проверяемая строка', 20);
 isShortString('проверяемая строка', 18);
 isShortString('проверяемая строка', 10);
 
 const isPalindrom = (string) => {
-  let phrase = string.replaceAll(' ', '').toLowerCase();
-  let reversePhrase = "";
-  for (let i = phrase.length-1; i >= 0; i--){
+  const phrase = string.replaceAll(' ', '').toLowerCase();
+  let reversePhrase = '';
+  for (let i = phrase.length - 1; i >= 0; i--){
     reversePhrase += phrase[i];
   }
-return reversePhrase === phrase ? "Палиндром" : "Не палиндром"
-}
+  return reversePhrase === phrase ? 'Палиндром' : 'Не палиндром';
+};
 isPalindrom('Лёша на полке клопа нашёл ');
