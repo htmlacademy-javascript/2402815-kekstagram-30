d76afc3e8e191440dabb1dac9c092f36b1477112
hect11c <svetlana1korolkova@gmail.com>
2023-10-31 22:38:36 +0300

    Добавлен обработчик клика по картинке-миниатюре

--- js/photo.js
+++ js/photo.js
@@ -1,12 +1,20 @@
+import {showBigPicture} from './show-picture';
+
 const templatePicture = document.querySelector('#picture').content.querySelector('.picture');
 const listOfPictures = document.querySelector('.pictures');
 
-const createTemplate = ({url, description, likes, comments}) => {
+const createTemplate = (picture) => {
+  const {url, description, likes, comments} = picture;
   const pictureElement = templatePicture.cloneNode(true);
   pictureElement.querySelector('.picture__img').src = url;
   pictureElement.querySelector('.picture__img').alt = description;
   pictureElement.querySelector('.picture__likes').textContent = likes;
   pictureElement.querySelector('.picture__comments').textContent = comments.length;
+  const onPictureElementClick = (evt) => {
+    evt.preventDefault();
+    showBigPicture(picture);
+  };
+  pictureElement.addEventListener('click', onPictureElementClick);
   return pictureElement;
 };
 
