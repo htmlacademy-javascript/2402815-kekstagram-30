9401b9da1fc982a51e743567c57a4df59675efcc
Keks <keks@htmlacademy.ru>
2023-11-09 18:28:13 +0300

    :heavy_check_mark: Сборка #9

---
+++ 9/js/main.js
@@ -0,0 +1,9 @@
+import {renderGallery} from './gallery.js';
+import {createPhotoArray} from './data.js';
+
+renderGallery(createPhotoArray());
+
+//import {fillTemplates} from './photo.js';
+//fillTemplates(createPhotoArray());
+
+
---
+++ 9/js/util-generating.js
@@ -0,0 +1,9 @@
+const getRandomInteger = (minValue, maxValue) => {
+  const lower = Math.ceil(Math.min(minValue, maxValue));
+  const upper = Math.floor(Math.max(minValue, maxValue));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
+};
+const getRandomArrayElement = (elements) => elements[getRandomInteger(0, elements.length - 1)];
+
+export {getRandomInteger, getRandomArrayElement};
