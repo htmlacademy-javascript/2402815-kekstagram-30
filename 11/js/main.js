23650eb58400405a0433416cb183f46ed2455fc4
Keks <keks@htmlacademy.ru>
2023-11-21 11:24:45 +0300

    :heavy_check_mark: Сборка #11

---
+++ 11/js/main.js
@@ -0,0 +1,10 @@
+import {renderGallery} from './gallery.js';
+import {createPhotoArray} from './data.js';
+import {showForm} from './form.js';
+
+renderGallery(createPhotoArray());
+showForm();
+//import {fillTemplates} from './photo.js';
+//fillTemplates(createPhotoArray());
+
+
